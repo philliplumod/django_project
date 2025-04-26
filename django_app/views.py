461fed4bd37d0796a8f686d105496b781607ed62
Phillip Jhon Lumod <philliplumod@gmail.com>
2025-04-26 23:09:22 +0800

    Add home view and update URL routing; configure settings and enable REST framework

--- django_app/views.py
+++ django_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+
+
+def home(request):
+    return HttpResponse(status.HTTP_200_OK)
+
 
 # Create your views here.
